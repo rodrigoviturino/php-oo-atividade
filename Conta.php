cfc660f522468c5efc8502e737354e9b891071bc
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-04 18:15:01 -0200

    Adicionando na class Conta get e set do atributo Endereco

--- Conta.php
+++ Conta.php
@@ -1,8 +1,17 @@
 <?php
 
 class Conta{
-    
+
     private $numero;
     public $cliente;
+    public $endereco;
 
+
+    public function getEndereco(){
+        return $this->endereco;
+    }
+
+    public function setEndereco($valor){
+        $this->endereco = $valor;
+    }
 }
