f184dd1a2bf3435bb88a08a4f6f9252af7608687
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-06 00:15:21 -0200

    Utilizando variaveis globais php

--- olamundo.php
+++ olamundo.php
@@ -1,3 +1,11 @@
 <?php
 
-echo "Olá mundo <br> Seja Bem-Vindo ao mundo php!";
+$nome = "Rodrigo";
+$sobrenome = "Viturino";
+$endereco = "Rua paulista";
+$idade = 24;
+$telefone = "1234-5678";
+
+// echo "Olá mundo <br> Seja Bem-Vindo ao mundo php!";
+
+echo "Sou o " . $nome . $sobrenome . " , tenho " . $idade . " anos e  faço curso no endereço:" . $endereco . " e o numero de contato do curso é: " . $telefone; 
