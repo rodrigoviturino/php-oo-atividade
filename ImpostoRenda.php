604fcabc860c697f350a31ed07f8937c7a9f4c8c
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-07 01:03:25 -0200

    Refaturando codigo com if e elseif com dois pontos

--- ImpostoRenda.php
+++ ImpostoRenda.php
@@ -1,15 +1,14 @@
 <?php
 
-$salario= 2000;
+$salario= 1501;
 
 
-if($salario <= 1000){
+if($salario <= 1000):
     echo "Uau! você não vai pagar nada de imposto!";
-} elseif ($salario <= 1500) {
+elseif ($salario <= 1500):
     echo "Puts! você precisa pagar 10% de imposto";
-} elseif ($salario <= 2000){
+elseif ($salario <= 2000):
     echo "Puts! voce vai ter pagar 15% de imposto";
-} else{
+else:
     echo "Puts! voce terá que pagar 20% de imposto";
-}
-
+endif;
