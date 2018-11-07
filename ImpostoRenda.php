d0a07db5cc9faf3e1b63e247dedaa22c5b75d179
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-07 00:50:01 -0200

    Iniciando com elseif

---
+++ ImpostoRenda.php
@@ -0,0 +1,15 @@
+<?php
+
+$salario= 2000;
+
+
+if($salario <= 1000){
+    echo "Uau! você não vai pagar nada de imposto!";
+} elseif ($salario <= 1500) {
+    echo "Puts! você precisa pagar 10% de imposto";
+} elseif ($salario <= 2000){
+    echo "Puts! voce vai ter pagar 15% de imposto";
+} else{
+    echo "Puts! voce terá que pagar 20% de imposto";
+}
+
