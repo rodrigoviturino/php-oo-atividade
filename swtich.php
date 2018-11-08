5a908953c68db5ce7fb9e34008dfb8af1bebb526
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-08 01:33:12 -0200

    Substituindo o if,elseif por swtich

---
+++ swtich.php
@@ -0,0 +1,23 @@
+<?php
+
+$opção = 3;
+
+switch ($opção) {
+    case(1):
+        echo "Você escolheu a opção 1 do cardápio.";
+        break;
+    case(2):
+        echo "Você escolheu a opção 2 do cardápio.";
+        break;
+    case(3):
+        echo "Você escolheu a opção 3 do cardápio.";
+        break;
+    case(4):
+        echo "Você escolheu a opção 4 do cardápio.";
+        break;
+    case(5):
+        echo "Você escolheu a opção 5 do cardápio.";
+        break;
+        
+            
+}
