6bced8db160d75f609b7f139d37ab1822bbbc702
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-10 18:20:35 -0200

    Listando total de itens 20 utilizando estrutura de repetição While com condicional if

---
+++ lista/personalidades.php
@@ -0,0 +1,31 @@
+<?php
+
+$personalidades = array(
+"Maom",
+"Isaac Newton",
+"Jesus Cristo",
+"Buda",
+"Confúcio",
+"Paulo de Tars",
+"Cai Lun",
+"Johannes Gutenberg",
+"Cristóvão Colombo",
+"Albert Einstein",
+"Louis Pasteur",
+"Galileu Galilei",
+"Aristóteles",
+"Euclides",
+"Moisés",
+"Charles Darwin",
+"Qin Shi Huan",
+"Augusto César",
+"Nicolau Copérnico",
+"Antoine Laurent Lavoisier",
+"Constantino, o Grande",
+"James Watt",
+"Michael Faraday",
+);
+
+foreach ($personalidades as $personalidade){
+    echo $personalidade . "<br>";
+}
