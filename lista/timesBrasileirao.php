bae351b501cd6585ce54f0f7f6eaa092d3f01856
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-10 17:55:56 -0200

    Listando times do brasileirao utlizando o foreach

---
+++ lista/timesBrasileirao.php
@@ -0,0 +1,29 @@
+<?php
+
+$times = array(
+    "Palmeiras",
+    "Internacional",
+    "Flamengo",
+    "São Paulo",
+    "Grêmio",
+    "Atlético-MG",
+    "Santos",
+    "Cruzeiro",
+    "Atlético-PR",
+    "Fluminense",
+    "Corinthians",
+    "Bahia",
+    "Vasco",
+    "Botafogo",
+    "Ceará",
+    "Sport",
+    "América-MG",
+    "Vitória",
+    "Chapecoense",
+    "Paraná",
+);
+
+foreach($times as $time){
+    echo $time . "<br>";
+
+}
