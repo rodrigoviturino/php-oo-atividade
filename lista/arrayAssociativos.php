43bed48fb95517346615733f559b0fc934e32408
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-11 15:54:10 -0200

    Listando array fazendo associação

---
+++ lista/arrayAssociativos.php
@@ -0,0 +1,25 @@
+<?php
+
+$cidade = array(
+    "nome" => "São Paulo",
+    "aniversario" => "25/01/1554",
+    "fundacao" => 1554,
+    "gentilico" => "Paulista",
+    "lema" => "Não sou conduzido, conduzo!",
+    "padroeiro" => "São Paulo",
+    "prefeito" => "Bruno Covas",
+);
+
+echo "Cidade: " . $cidade["nome"];
+echo "<br>";
+echo "Data de nascimento: " . $cidade["aniversario"];
+echo "<br>";
+echo "Fundação: ". $cidade["fundacao"];
+echo "<br>";
+echo "Gentilico: " . $cidade["gentilico"];
+echo "<br>";
+echo "Lema: " . $cidade["lema"];
+echo "<br>";
+echo "Padroeiro: " . $cidade["padroeiro"];
+echo "<br>";
+echo "Prefeito: " . $cidade["prefeito"]; 
