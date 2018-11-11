b60a39bbb1fd05856fdea04bce05f31858b97f12
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-11 17:17:48 -0200

    Criando formulario e recebendo dados via GET

---
+++ formulario/cadastro_aluno.php
@@ -0,0 +1,16 @@
+<form action="processa_form.php" method="get">
+
+<label for="nome">Nome:</label>
+<input type="text" id="nome" name="nome"><br>
+
+<label for="sobrenome">Sobrenome:</label>
+<input type="text" id="sobrenome" name="sobrenome"><br>
+ 
+<label for="email">Email:</label>
+<input type="mail" id="email" name="email"><br>
+ 
+<label for="nascimento">Data Nascimento:</label>
+<input type="text" id="nascimento" name="nascimento"><br>
+ 
+<input type="submit" value="Enviar dados"><br>
+</form>
