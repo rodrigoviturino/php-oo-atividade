a6ad829e9b48a29efb76aa6790e9c0a008a645bc
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-16 00:33:36 -0200

    Criando formulario que manda dados via POST e ao receber os dados, mostrar quais estão recebendo

---
+++ formulario_tarefas/form_usuario.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar Usuario</title>
+</head>
+<body>
+
+<form action="usuario_post.php" method="post">
+
+    <label>EMAIL</label><br>
+    <input type="text" name="email"><br>
+
+    <label>SENHA</label><br>
+    <input type="text" name="senha"><br><br>
+
+    <input type="submit">
+
+</form>
+    
+</body>
+</html>
