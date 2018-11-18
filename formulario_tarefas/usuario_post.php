8c3ddc8855ea290bc848f9a7233850e44f766594
Rodrigo Viturino <viturino.souza1@gmail.com>
2018-11-18 01:27:43 -0200

    Coletando dados do formulario usuario e inserindo no banco de dados

--- formulario_tarefas/usuario_post.php
+++ formulario_tarefas/usuario_post.php
@@ -1,4 +1,26 @@
 <?php
 
-echo "Email: " . $_POST["email"] . "<br>";
-echo "Senha: " . $_POST["senha"];
+$email = $_POST["email"];
+$senha = $_POST["senha"];
+
+$servidor = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "curso_phpoo";
+
+$conn = new mysqli($servidor, $usuario, $senha, $banco);
+
+if($conn->connect_error){
+    die("Erro ao conectar no banco " . $conn->error);
+}
+
+$sql = " INSERT INTO usuarios (email,senha) VALUES";
+$sql .= "('$email','$senha')";
+
+if($conn->query($sql) != 0 ){
+    echo "Dados inseridos com sucesso!<br>";
+} else {
+    echo "Erro ao tentar inserir dados: " . $sql . "<br>". $conn->connect_error;
+}
+
+$conn->close();
